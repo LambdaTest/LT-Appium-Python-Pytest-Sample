dc10191c31eaa51151248b95f8cf1df5fcbe4f93
UchihaMadaraaa <karang@lambdatest.com>
2025-05-28 13:11:00 +0530

    TTN-23587 minor changes to get the test running

--- conftest.py
+++ conftest.py
@@ -1,21 +1,24 @@
 from os import environ
 import pytest
 from appium import webdriver
+from appium.options.android import UiAutomator2Options
 from requests import request
 
 @pytest.fixture(scope='function')
 def test_setup(request):
     test_name = request.node.name
     build = environ.get('BUILD', "Sample PY Build")
-    caps = {}
-    caps["deviceName"] = "Galaxy S21 Ultra 5G"
-    caps["platformName"] = "Android"
-    caps["platformVersion"] = "11"
-    caps["app"] = "lt://APP10020521645537467103592"
-    caps["isRealMobile"] = True
-    caps['build'] = build
-    caps['name'] = test_name
-    driver = webdriver.Remote("https://<Username>:<AccessKey>@mobile-hub.lambdatest.com/wd/hub", caps)
+    
+    options = UiAutomator2Options()
+    options.set_capability("deviceName", "Galaxy S21 Ultra 5G")
+    options.set_capability("platformName", "Android")
+    options.set_capability("platformVersion", "11")
+    options.set_capability("app", "lt://APP10160301691747362768822447")
+    options.set_capability("isRealMobile", True)
+    options.set_capability("build", build)
+    options.set_capability("name", test_name)
+    
+    driver = webdriver.Remote("https://<Username>:<AccessKey>@mobile-hub.lambdatest.com/wd/hub", options=options)
     request.cls.driver = driver
     
     yield driver
